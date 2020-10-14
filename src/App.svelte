c3c93b0d7ed07336edc099fea77566089d31bf21
José Hidalgo <josehidalgo990@gmail.com>
2020-10-14 16:39:26 -0400

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+</script>
+
+<style>
+</style>
+
+<main>
+  <form>
+    <input type="text" placeholder="Product Name" />
+    <textarea
+      id="product-description"
+      rows="3"
+      placeholder="Product Description" />
+    <input
+      type="url"
+      id="product-image-url"
+      placeholder="https://imjosehidalgo.dev/" />
+    <button>Save Product</button>
+  </form>
+</main>
